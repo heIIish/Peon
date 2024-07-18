906df1b1d8177e9c916fb3e889402ee494617c4f
heIIish <44658987+heIIish@users.noreply.github.com>
2024-07-18 03:44:13 -0400

    AutoFood safety checks

--- SND/Scripts/AutoFood.lua
+++ SND/Scripts/AutoFood.lua
@@ -7,8 +7,11 @@ local Status = require "Status"
 local BoiledEgg = 4650
 
 while true do
-	if GetCharacterCondition(Condition.BoundByDuty) and GetStatusTimeRemaining(Status.WellFed) < 60 and GetItemCount(BoiledEgg) > 0 then
+	if GetCharacterCondition(Condition.BoundByDuty)
+	and not GetCharacterCondition(Condition.InCombat)
+	and GetStatusTimeRemaining(Status.WellFed) < 1200
+	and GetItemCount(BoiledEgg) > 0 then
 		yield("/item \"Boiled Egg\"")
 	end
-	wait(10)
+	wait(5)
 end
